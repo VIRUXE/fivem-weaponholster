f3d35437ef45f63f489993f8181df7d4f93c2cf4
Senlar <brandonrhue@gmail.com>
2022-02-19 17:37:50 -0500

    (rewrite) Major code refactor
    Rewrote to change from a while loop to event listener for major optimization. Removed need to search items and loop to watch for item changes. Fixed issue causing inventory to choose random weapon if player had multiple weapons from the weapon list. Built a possible fix for #1. Also left a conditional for people to build in requirements for getting weapons from vehicles if they want.

--- client.lua
+++ client.lua
@@ -1,8 +1,8 @@
-local weapons =
+local weaponData =
 { -- Thanks to https://github.com/itzhapp/ShowWeapon
     ['WEAPON_ADVANCEDRIFLE']      = {model = `w_ar_advancedrifle`,      onBack = true},
     ['WEAPON_ASSAULTRIFLE_MK2']   = {model = `w_ar_assaultrifle`,       onBack = true},
-    ['WEAPON_ASSAULTRIFLE']       = {model = `w_ar_assaultrifle`,       onBack = false},
+    ['WEAPON_ASSAULTRIFLE']       = {model = `w_ar_assaultrifle`,       onBack = true},
     ['WEAPON_ASSAULTSHOTGUN']     = {model = `w_sg_assaultshotgun`,     onBack = true},
     ['WEAPON_ASSAULTSMG']         = {model = `w_sb_assaultsmg`,         onBack = true},
     ['WEAPON_BULLPUPRIFLE_MK2']   = {model = `w_ar_bullpuprifle`,       onBack = true},
@@ -37,48 +37,43 @@ local weapons =
 }
 
 local weaponHolstered = {name = nil, object = nil}
+local weaponEquipped = {name = nil, object = nil}
 
-CreateThread(function()
-	while true do
-		local playerPed = PlayerPedId()
+AddEventHandler('playerDropped', function()
+    --Haven't tested this, if it doesn't work might need to handle it on the server side with a netID
+    DeleteObject(weaponholstered.object)
+end)
 
-		-- Player shouldn't be shuffling weapons if dead so just don't do anything
-		if IsEntityDead(playerPed) or not ESX.IsPlayerLoaded() then goto continue end
+AddEventHandler('ox_inventory:currentWeapon',function(currentWeapon)
+    local playerPed = PlayerPedId()
+    if not currentWeapon then 
+        if weaponEquipped and weaponEquipped.name then 
+            if weaponData[weaponEquipped and weaponEquipped.name] and weaponData[weaponEquipped.name].onBack == true then
+                if weaponHolstered.object then DeleteObject(weaponHolstered.object) end
+                    while not HasModelLoaded(weaponData[weaponEquipped.name].model) do
+                        RequestModel(weaponData[weaponEquipped.name].model)
+                        Wait(0)
+                    end
 
-		-- Nothing holstered so let's search our inventory for weapons
-		if not weaponHolstered.object then
-			-- Only check the inventory if we're not inside a vehicle or already armed
-			if not IsPedInAnyVehicle(playerPed, false) and not IsPedArmed(playerPed, 4) then
-				-- Loop trough all weapons to see if we have any in the inventory
-				for weapon, weaponData in pairs(weapons) do
-					-- We found a weapon so let's holster it correctly and not search for something else
-					if exports.ox_inventory:Search('count', weapon) > 0 then
-						-- Let's load the model first because we are nice like that
-						while not HasModelLoaded(weaponData.model) do
-							RequestModel(weaponData.model)
-							Citizen.Wait(0)
-						end
+                    weaponHolstered.name = weaponEquipped.name
+                    weaponHolstered.object = CreateObject(weaponData[weaponEquipped.name].model, 0, 0, 0, true, true, true)
 
-						weaponHolstered.name = weapon
-						weaponHolstered.object = CreateObject(weaponData.model, 0, 0, 0, true, true, true)
-
-						SetModelAsNoLongerNeeded(weaponData.model)
-
-						AttachEntityToEntity(weaponHolstered.object, playerPed, GetPedBoneIndex(playerPed, 24816), weaponData.onBack and 0.3 or 0.2, weaponData.onBack and -0.15 or 0.20, weaponData.onBack and 0.05 or -0.1, weaponData.onBack and 0.0 or 180.0, weaponData.onBack and 0.0 or 220.0, 0.0, true, true, false, false, 1, true)
-						break
-					end
-				end
-			end
-		else
-			-- A weapon is holstered so delete the object of it's no longer in the inventory, we are using it or in a vehicle
-			if exports.ox_inventory:Search('count', weaponHolstered.name) == 0 or IsPedArmed(playerPed, 4) or IsPedInAnyVehicle(playerPed, false) then
-				DeleteObject(weaponHolstered.object)
-				weaponHolstered.name = nil
-				weaponHolstered.object = nil
-			end
-		end
-
-		::continue::
-		Wait(1000)
-	end
+                    SetModelAsNoLongerNeeded(weaponData[weaponEquipped.name].model)
+                    AttachEntityToEntity(weaponHolstered.object, playerPed, GetPedBoneIndex(playerPed, 24816), weaponData[weaponEquipped.name].onBack and 0.3 or 0.2, weaponData[weaponEquipped.name].onBack and -0.15 or 0.20, weaponData[weaponEquipped.name].onBack and 0.05 or -0.1, weaponData[weaponEquipped.name].onBack and 0.0 or 180.0, weaponData[weaponEquipped.name].onBack and 0.0 or 220.0, 0.0, true, true, false, false, 1, true)
+                end
+            end
+        end
+    elseif currentWeapon and currentWeapon.name == weaponHolstered.name then
+	--Taking holstered weapon off back to use it
+        weaponEquipped.name = currentWeapon.name
+        DeleteObject(weaponHolstered.object)
+        weaponHolstered.name = nil
+        weaponHolstered.object = nil
+    elseif weaponData[currentWeapon.name] and weaponData[currentWeapon.name].onBack == true then 
+	--Leaves room for people to build additional logic for onback weapons like requiring them to be taken from a vehicle 
+        weaponEquipped.name = currentWeapon.name
+    else
+	--Catch all for weapons not defined in the weaponData list. Just allow them to be equipped
+        weaponEquipped.name = currentWeapon.name
+    end
 end)
